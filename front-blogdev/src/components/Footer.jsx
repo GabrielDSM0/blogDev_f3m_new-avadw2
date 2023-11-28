a13207756f7cb0ffb76e3a191185d63e7d76c12e
GabrielDSM0 <gabrielcamargonorberto@outlook.com>
2023-11-28 14:58:38 -0300

    Avaliação: Gabriel Camargo Norberto

--- front-blogdev/src/components/Footer.jsx
+++ front-blogdev/src/components/Footer.jsx
@@ -5,7 +5,6 @@ const Footer = () => {
   return (
    <>
    <footer className={styles.footer}>
-    <h3>Blog da Vida do Jhow Desenvolvedor</h3>
     <p>Blog Dev &copy; 2023</p>
    </footer>
    </>
